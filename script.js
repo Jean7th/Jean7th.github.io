3a6c23b17cd3b574d859c4d72082c858f9c7f074
Jirat <jirat7439@hotmail.com>
2024-09-25 17:22:10 +0100

    Updated

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+// Select the full-name from HTML
+const fullName = document.querySelector('#full-name');
+
+// Add eventListener to full-name
+fullName.addEventListener('click', changeName);
+
+//Change Name Language
+function changeName() {
+    if (fullName.innerText == "Jirat Innkaew")
+        fullName.innerText = "จิรัฏฐ์ อินทร์แก้ว";
+    else fullName.innerText = "Jirat Innkaew";
+    
+}
+
+
+
